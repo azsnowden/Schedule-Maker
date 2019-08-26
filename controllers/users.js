1cebec027c4941a8b45d7bf4fccd520f179c3927
Ben Davenport <benjamin.p.davenport@gmail.com>
2019-08-26 15:55:42 -0400

    Added user profile and employee list page

--- controllers/users.js
+++ controllers/users.js
@@ -4,10 +4,11 @@ const db = require('../db');
 //Display profile
 async function profile(userId){
     try {
-        const user = await db.one('select * from users WHERE id=$1'[userId]);
-        const userBusiness = await db.any('select * from businesses where user_business_id=$1', [user.business_id]);
-        user.business = userBusiness;
-        console.log(user)
+        const user = await db.one('select * from users WHERE id=$1',[userId]);
+        const userBusiness = await db.any('select * from businesses where id=$1', [user.business_id]);
+        user.business = userBusiness[0];
+        // console.log(userBusiness)
+        // console.log(user)
         return user
         }
     catch(error){
