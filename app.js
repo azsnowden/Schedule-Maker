09d4ff7ee9fd1b7bfa1370fe0f3f2e301051333d
Ben Davenport <benjamin.p.davenport@gmail.com>
2019-09-03 15:51:04 -0400

    great update of callback

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const strategy = new auth0Strategy(
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
     callbackURL:
-      process.env.AUTH0_CALLBACK_URL || '/callback'
+      process.env.AUTH0_CALLBACK_URL || 'https://scheduler.bendavenport.co/callback/'
   },
   function (accessToken, refreshToken, extraParams, profile, done) {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
